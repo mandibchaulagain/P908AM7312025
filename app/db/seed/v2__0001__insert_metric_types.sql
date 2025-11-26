621e0794d83be5391c8e4d9c33cdd80f440b47a1
mandibchaulagain <mandibchaulagain8@gmail.com>
2025-11-26 16:42:44 +0545

    v2 seeding metric types

---
+++ app/db/seed/v2__0001__insert_metric_types.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO metric_types (name, unit, data_type) VALUES
+    ('nitrogen', 'ppm', 'float'),
+    ('phosphorus', 'ppm', 'float'),
+    ('potassium', 'ppm', 'float'),
+    ('temperature', '°C', 'float'),
+    ('rainfall', 'mm', 'float'),
+    ('humidity', '%', 'float');
